3806bfb1cf3f3e13fae6acd60a2c0713725caaf8
Gaetano Checinski <Gaetano.Checinski@gmail.com>
2015-05-06 20:46:26 +0200

    more convienience functions added

--- include/gen/helpers.hpp
+++ include/gen/helpers.hpp
@@ -12,8 +12,31 @@ auto Wrap(F f) {
   return gen*W<F>{ f };
 }
 
+static auto each = [](auto F){
+  return Wrap<Each>(F);
+};
+
+static auto map = [](auto F){
+  return Wrap<Map>(F);
+};
+
+static auto take = [](auto...X){
+  return Wrap<Take>(X...);
+};
+
+static auto takeWhile = [](auto...X){
+  return Wrap<TakeWhile>(X...);
+};
+
+static auto dropWhile = [](auto...X){
+  return Wrap<DropWhile>(X...);
+};
+
+static auto range = [](auto...X){
+  return gen*Range(X...);
+};
+
+
 }
-
-
 
 #endif
